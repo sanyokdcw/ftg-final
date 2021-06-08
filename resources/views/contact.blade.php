7006909d1198e3a87fb6a7e5170134c778e54c0b
Sanyok <useriser21@gmail.com>
2021-06-08 15:13:18 +0600

    footer

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -15,7 +15,7 @@
   <div class="contact__title subtitle">Контакты</div>
   <div class="contact__wrapper">
     <div class="contact__wrapper-left">
-      <div class="contact__wrapper-title subtitle">АДРЕС:</div>
+      <div class="contact__wrapper-title">АДРЕС:</div>
       <div class="contact__wrapper-text">
         {!! setting('contacts.address') !!}
       </div>
@@ -28,7 +28,7 @@
         {!! setting('contacts.schedule') !!}
       </div>
     </div>
-    <div class="contact__wrapper-right">
+    <div class="contact__wrapper-right" style="width: 69%;">
       <iframe
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2539.150735195916!2d30.50318721595899!3d50.47553749343766!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40d4ce0dcdf7692b%3A0x10f8683c94bed4d5!2z0YPQuy4g0J3QsNCx0LXRgNC10LbQvdC-LdCb0YPQs9C-0LLQsNGPLCAzMiwg0JrQuNC10LIsINCj0LrRgNCw0LjQvdCwLCAwMjAwMA!5e0!3m2!1sru!2sru!4v1620246695071!5m2!1sru!2sru"
         width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy">
