d068ca4fe46d40ad4a1fc22cd3cb55e53602db51
Sanyok <useriser21@gmail.com>
2021-06-08 10:43:11 +0600

    adaptive

--- resources/views/layouts/catalog.blade.php
+++ resources/views/layouts/catalog.blade.php
@@ -1,6 +1,6 @@
 <head>
   <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta name="viewport" content="width=device-width, initial-scale=0.8" />
   <title>FTG</title>
   <link rel="stylesheet" href="../css/style.min.css" />
 </head>
